f5b914326d567de796ddad49ee538b112b3c31ac
Robert Szekely <szekelyrobert1995@gmail.com>
2019-02-05 01:08:22 +0200

    Add application class

---
+++ app/src/main/java/com/example/booklibrary/BookLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.booklibrary
+
+import android.app.Application
+
+@Suppress("unused")
+class BookLibraryApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
