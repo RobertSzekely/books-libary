bd71729fa08735f4582625b352e29884ceb1f244
Robert Szekely <szekelyrobert1995@gmail.com>
2019-02-05 19:35:38 +0200

    Setup bottom navigation

--- app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
+++ app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
@@ -6,19 +6,40 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.booklibrary.R
 import com.example.booklibrary.databinding.HomeActivityBinding
+import com.example.booklibrary.feature.favorites.FavoritesFragment
 import com.example.booklibrary.feature.feed.BookFeedFragment
+import com.example.booklibrary.feature.wishlist.WishlistFragment
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<HomeActivityBinding>(this, R.layout.activity_home)
+
+        DataBindingUtil.setContentView<HomeActivityBinding>(this, R.layout.activity_home).apply {
+            setSupportActionBar(this.toolbar)
+            bottomNavigation.setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.home -> {
+                        navigateToFragment(BookFeedFragment.newInstance())
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.favorites -> {
+                        navigateToFragment(FavoritesFragment.newInstance())
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                    R.id.wishlist -> {
+                        navigateToFragment(WishlistFragment.newInstance())
+                        return@setOnNavigationItemSelectedListener true
+                    }
+                }
+                false
+            }
+        }
 
         if (savedInstanceState == null) {
             navigateToFragment(BookFeedFragment.newInstance())
         }
 
-        setSupportActionBar(binding.toolbar)
     }
 
     private fun navigateToFragment(fragment: Fragment) {
