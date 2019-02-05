6e2c14478b633db32c4637e6092d995978f6d5fc
Robert Szekely <szekelyrobert1995@gmail.com>
2019-02-05 15:09:07 +0200

    Add material theme and update app style

--- app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
+++ app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
@@ -3,14 +3,28 @@ package com.example.booklibrary.feature
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.booklibrary.R
 import com.example.booklibrary.databinding.HomeActivityBinding
+import com.example.booklibrary.feature.feed.BookFeedFragment
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<HomeActivityBinding>(this, R.layout.activity_home)
+
+        if (savedInstanceState == null) {
+            navigateToFragment(BookFeedFragment.newInstance())
+        }
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    private fun navigateToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.main_content, fragment)
+                .commit()
     }
 
 }
