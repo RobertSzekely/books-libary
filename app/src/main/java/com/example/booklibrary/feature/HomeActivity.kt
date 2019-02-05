466da7f81bb4607c15d5f94a3424be0b250ca780
Robert Szekely <szekelyrobert1995@gmail.com>
2019-02-05 12:37:59 +0200

    Add bottom navigation to home activity

--- app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
+++ app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
@@ -2,13 +2,15 @@ package com.example.booklibrary.feature
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil
 import com.example.booklibrary.R
+import com.example.booklibrary.databinding.HomeActivityBinding
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = DataBindingUtil.setContentView<HomeActivityBinding>(this, R.layout.activity_home)
     }
 
 }
