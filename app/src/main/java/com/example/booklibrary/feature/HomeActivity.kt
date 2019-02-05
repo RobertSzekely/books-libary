e47d122506d5ad3449a54a7a86cd61b9b13be25b
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-05 12:07:13 +0200

    Refactor home activity

---
+++ app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.booklibrary.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.booklibrary.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
