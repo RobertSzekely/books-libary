e4cf310139a5ca571479529da1e9aab5791a750b
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-06 17:57:57 +0200

    Setup adapter and observe data in book feed fragment

--- app/src/main/java/com/example/booklibrary/feature/feed/BookFeedFragment.kt
+++ app/src/main/java/com/example/booklibrary/feature/feed/BookFeedFragment.kt
@@ -1,19 +1,19 @@
 package com.example.booklibrary.feature.feed
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.booklibrary.BookFeedFragmentBinding
-
 import com.example.booklibrary.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BookFeedFragment : Fragment() {
 
-    private lateinit var viewModel: BookFeedViewModel
+    private val viewModel: BookFeedViewModel by viewModel()
     private lateinit var binding: BookFeedFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -23,8 +23,22 @@ class BookFeedFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(BookFeedViewModel::class.java)
-        // TODO: Use the ViewModel
+        val adapter = BookAdapter()
+        with(binding) {
+            viewModel = this@BookFeedFragment.viewModel
+            recycler.adapter = adapter
+            setLifecycleOwner(viewLifecycleOwner)
+        }
+        viewModel.booksList.observe(viewLifecycleOwner, Observer {result ->
+            if (!result.isNullOrEmpty()) {
+                adapter.submitList(result)
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadBooks()
     }
 
     companion object {
