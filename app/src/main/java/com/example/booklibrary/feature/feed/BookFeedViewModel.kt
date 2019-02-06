d2f667b555a52ea909da49f8ecad6255dc68b5cd
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-06 17:57:25 +0200

    Handle load data in book feed view model

--- app/src/main/java/com/example/booklibrary/feature/feed/BookFeedViewModel.kt
+++ app/src/main/java/com/example/booklibrary/feature/feed/BookFeedViewModel.kt
@@ -1,7 +1,25 @@
 package com.example.booklibrary.feature.feed
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.booklibrary.data.models.Book
+import com.example.booklibrary.data.storage.BookRepository
 
-class BookFeedViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class BookFeedViewModel(private val repository: BookRepository) : ViewModel() {
+
+    val booksList = MutableLiveData<List<Book>>()
+
+    fun loadBooks() {
+        repository.getBooks(object : BookRepository.ApiCallback {
+            override fun onSuccess(books: List<Book>) {
+                booksList.postValue(books)
+            }
+
+            override fun onFailure(errorMessage: String) {
+                Log.d("BookFeedViewModel", "Loading books failed!")
+            }
+
+        })
+    }
 }
