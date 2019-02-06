170ed861f86c507816f2a897bc3196a2609e9249
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-06 17:52:44 +0200

    Add book response and book model

---
+++ app/src/main/java/com/example/booklibrary/data/models/Book.kt
@@ -0,0 +1,3 @@
+package com.example.booklibrary.data.models
+
+class Book(val id: Int, val title: String, val author: String, val cover: String)
---
+++ app/src/main/java/com/example/booklibrary/data/models/BooksResponse.kt
@@ -0,0 +1,6 @@
+package com.example.booklibrary.data.models
+
+import com.example.booklibrary.data.models.Book
+
+
+class BooksResponse (val count: Int, val books: List<Book>)
