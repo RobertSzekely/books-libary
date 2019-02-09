ca135ce5a9ea5b2b874420f717c535069c70729c
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-10 00:35:33 +0200

    Write binding adapter for loading image with glide

--- app/src/main/java/com/example/booklibrary/feature/BindingAdapter.kt
+++ app/src/main/java/com/example/booklibrary/feature/BindingAdapter.kt
@@ -1,22 +1,11 @@
 package com.example.booklibrary.feature
 
-import android.annotation.SuppressLint
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-import com.example.booklibrary.R
 
-@SuppressLint("CheckResult")
+
 @BindingAdapter("imageUrl")
-fun ImageView.loadImage(url: String?) {
-    val requestOptions = RequestOptions().apply {
-        transforms(CenterCrop(), RoundedCorners(30))
-        fallback(R.drawable.no_photo)
-        placeholder(R.drawable.no_photo)
-        error(R.drawable.no_photo)
-    }
-    Glide.with(context).load(url).apply(requestOptions).into(this)
+fun setImageUrl(imageView: ImageView, url: String?) {
+    Glide.with(imageView.context).load(url).into(imageView)
 }
