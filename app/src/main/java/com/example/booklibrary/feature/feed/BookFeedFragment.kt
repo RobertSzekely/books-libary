6676a9d33e39df100501bba59a4d4ca1d600951c
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-09 21:40:44 +0200

    Set layout manager for recycler

--- app/src/main/java/com/example/booklibrary/feature/feed/BookFeedFragment.kt
+++ app/src/main/java/com/example/booklibrary/feature/feed/BookFeedFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.booklibrary.BookFeedFragmentBinding
 import com.example.booklibrary.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -27,6 +28,7 @@ class BookFeedFragment : Fragment() {
         with(binding) {
             viewModel = this@BookFeedFragment.viewModel
             recycler.adapter = adapter
+            recycler.layoutManager = LinearLayoutManager(context)
             setLifecycleOwner(viewLifecycleOwner)
         }
         viewModel.booksList.observe(viewLifecycleOwner, Observer {result ->
