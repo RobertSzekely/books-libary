9164ad51daa68251a6f7337beda9cebc9a540401
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-10 00:34:53 +0200

    Make book cover property nullable

--- app/src/main/java/com/example/booklibrary/data/models/Book.kt
+++ app/src/main/java/com/example/booklibrary/data/models/Book.kt
@@ -5,4 +5,4 @@ import com.google.gson.annotations.SerializedName
 class Book(@SerializedName("_id") val id: String,
            val title: String,
            val author: String,
-           val cover: String)
+           val cover: String?)
