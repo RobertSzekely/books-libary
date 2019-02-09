0082f0a4a351c1ccd6fe567ef477fd30b6a77d7e
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-09 21:43:25 +0200

    Rewrite BookAdapter

--- app/src/main/java/com/example/booklibrary/feature/feed/BookAdapter.kt
+++ app/src/main/java/com/example/booklibrary/feature/feed/BookAdapter.kt
@@ -1,47 +1,32 @@
 package com.example.booklibrary.feature.feed
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.booklibrary.data.models.Book
 import com.example.booklibrary.databinding.ItemBookBinding
+import com.example.booklibrary.feature.common.BaseListAdapter
 
-class BookAdapter : ListAdapter<Book, BookAdapter.ViewHolder>(BookDiffCallback()) {
+class BookAdapter : BaseListAdapter<Book, ItemBookBinding>(BOOK_DIFF_UTIL) {
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val book = getItem(position)
-        holder.apply {
-            bind(View.OnClickListener {  }, book)
-            itemView.tag = book
-        }
+    override fun createBinding(parent: ViewGroup): ItemBookBinding {
+        return ItemBookBinding.inflate(LayoutInflater.from(parent.context))
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(ItemBookBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun bind(binding: ItemBookBinding, item: Book) {
+        binding.book = item
     }
 
-    class ViewHolder(private val binding: ItemBookBinding) : RecyclerView.ViewHolder(binding.root) {
+    companion object {
+        val BOOK_DIFF_UTIL = object : DiffUtil.ItemCallback<Book>() {
 
-        fun bind(listener: View.OnClickListener, item: Book) {
-            binding.apply {
-                clickListener = listener
-                book = item
-                executePendingBindings()
+            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
+                return oldItem == newItem
             }
         }
-    }
-}
-
-private class BookDiffCallback : DiffUtil.ItemCallback<Book>() {
-
-    override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
-        return oldItem == newItem
     }
 }
