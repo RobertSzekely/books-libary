097bb5cef3bca0f4cd13c2c03dcdc52e3881a627
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-10 22:25:25 +0200

    Implement validate data and add book

--- app/src/main/java/com/robertszekely/booklibrary/feature/feed/addbook/AddBookActivity.kt
+++ app/src/main/java/com/robertszekely/booklibrary/feature/feed/addbook/AddBookActivity.kt
@@ -2,15 +2,43 @@ package com.robertszekely.booklibrary.feature.feed.addbook
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.robertszekely.booklibrary.AddBookBinding
 import com.robertszekely.booklibrary.R
+import com.robertszekely.booklibrary.data.models.Book
+import kotlinx.android.synthetic.main.activity_add_book.authorInput
+import kotlinx.android.synthetic.main.activity_add_book.coverInput
+import kotlinx.android.synthetic.main.activity_add_book.titleInput
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddBookActivity : AppCompatActivity() {
 
+    private val viewModel: AddBookViewModel by viewModel()
+    lateinit var binding: AddBookBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_book)
+        binding = DataBindingUtil.setContentView<AddBookBinding>(this, R.layout.activity_add_book).apply {
+            saveButton.setOnClickListener { validateAndAddBook() }
+        }
+    }
+
+    private fun validateAndAddBook() {
+        if (!binding.titleInput.text.isNullOrEmpty()) {
+            if (!binding.authorInput.text.isNullOrEmpty()) {
+                if (!binding.coverInput.text.isNullOrEmpty()) {
+                    viewModel.addBook(Book(null, titleInput.text.toString(), authorInput.text.toString(), coverInput.text.toString()))
+                } else {
+                    binding.textInputCover.error = resources.getString(R.string.add_book_error_cover)
+                }
+            } else {
+                binding.textInputAuthor.error = resources.getString(R.string.add_book_error_author)
+            }
+        } else {
+            binding.textInputTitle.error = resources.getString(R.string.add_book_error_title)
+        }
     }
 
     companion object {
