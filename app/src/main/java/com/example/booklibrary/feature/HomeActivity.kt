7acbfb06b8f3f587ffcc24641cd0ca3cfe12917d
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-10 02:53:04 +0200

    Replace bottom navigation with BottomAppBar

--- app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
+++ app/src/main/java/com/example/booklibrary/feature/HomeActivity.kt
@@ -1,7 +1,8 @@
 package com.example.booklibrary.feature
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.booklibrary.R
@@ -9,46 +10,60 @@ import com.example.booklibrary.databinding.HomeActivityBinding
 import com.example.booklibrary.feature.favorites.FavoritesFragment
 import com.example.booklibrary.feature.feed.BookFeedFragment
 import com.example.booklibrary.feature.wishlist.WishlistFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var bottomDrawerBehavior: BottomSheetBehavior<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         DataBindingUtil.setContentView<HomeActivityBinding>(this, R.layout.activity_home).apply {
-            setSupportActionBar(this.toolbar)
-            bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            setSupportActionBar(bottomAppBar)
+            bottomDrawerBehavior = BottomSheetBehavior.from(bottomDrawer)
+            bottomDrawerBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            bottomAppBar.setNavigationOnClickListener { bottomDrawerBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED }
+            bottomAppBar.replaceMenu(R.menu.main_navigation)
+            navigationView.setNavigationItemSelectedListener { item ->
                 when (item.itemId) {
                     R.id.home -> {
                         fab.show()
                         navigateToFragment(BookFeedFragment.newInstance())
-                        return@setOnNavigationItemSelectedListener true
+                        return@setNavigationItemSelectedListener true
                     }
                     R.id.favorites -> {
                         fab.hide()
                         navigateToFragment(FavoritesFragment.newInstance())
-                        return@setOnNavigationItemSelectedListener true
+                        return@setNavigationItemSelectedListener true
                     }
                     R.id.wishlist -> {
                         fab.hide()
                         navigateToFragment(WishlistFragment.newInstance())
-                        return@setOnNavigationItemSelectedListener true
+                        return@setNavigationItemSelectedListener true
                     }
                 }
                 false
             }
         }
-
         if (savedInstanceState == null) {
             navigateToFragment(BookFeedFragment.newInstance())
         }
-
     }
 
     private fun navigateToFragment(fragment: Fragment) {
+        bottomDrawerBehavior.state = BottomSheetBehavior.STATE_HIDDEN
         supportFragmentManager.beginTransaction()
-                .replace(R.id.main_content, fragment)
-                .commit()
+            .replace(R.id.main_content, fragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        if (bottomDrawerBehavior.state != BottomSheetBehavior.STATE_HIDDEN) {
+            bottomDrawerBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            return
+        }
+        super.onBackPressed()
     }
 
 }
