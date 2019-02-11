3e7301258eaca086ff87445559a77a90166505dc
Róbert Székely <szekelyrobert1995@gmail.com>
2019-02-11 18:18:18 +0200

    Implement detail fragment layout

--- app/src/main/java/com/robertszekely/booklibrary/feature/detail/DetailFragment.kt
+++ app/src/main/java/com/robertszekely/booklibrary/feature/detail/DetailFragment.kt
@@ -1,33 +1,31 @@
 package com.robertszekely.booklibrary.feature.detail
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.example.booklibrary.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.robertszekely.booklibrary.DetailFragmentBinding
 
 class DetailFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DetailFragment()
-    }
-
     private lateinit var viewModel: DetailViewModel
+    private lateinit var binding: DetailFragmentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    companion object {
+        fun newInstance() = DetailFragment()
     }
 
 }
